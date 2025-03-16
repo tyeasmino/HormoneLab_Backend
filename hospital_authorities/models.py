cfead95ce4ee1de3005d8d98ee0517b018505abc
Tamima Yeasmin <tyeasmino@gmail.com>
2025-03-17 04:02:34 +0600

    working on hospital authority profile

--- hospital_authorities/models.py
+++ hospital_authorities/models.py
@@ -6,7 +6,7 @@ from clients.models import Location
 class HospitalAuthority(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.URLField(max_length=255, blank=True, null=True)
-    location = models.OneToOneField(Location, blank=True, null=True, on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.DO_NOTHING)
     phone = models.CharField(max_length=11, null=True, blank=True)
     hospital_name = models.CharField(max_length=255, null=True, blank=True)
 
