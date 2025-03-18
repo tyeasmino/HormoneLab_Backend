bf3de1b7d5bab6c3e854a131924a369aedfa2ea6
Tamima Yeasmin <tyeasmino@gmail.com>
2025-03-18 11:50:46 +0600

    reports updated

--- clients/models.py
+++ clients/models.py
@@ -10,20 +10,21 @@ class Location(models.Model):
         return f"{self.location_name} - is selected" if self.is_selected else f"{self.location_name}"
 
 
+class Reports(models.Model):
+    hospital = models.ForeignKey("hospital_authorities.HospitalAuthority", on_delete=models.DO_NOTHING)
+    report_name = models.CharField(max_length=100, blank=True, null=True)
+    report_file = models.URLField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.created_at}: {self.report_name} report of {self.hospital.location.location_name} - {self.hospital.hostpital_name} hospital"
+    
+
+
+
 class Hospital(models.Model):
     location_name = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
     hostpital_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.location_name} - {self.hostpital_name}" 
-
-
-class Reports(models.Model):
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    report_name = models.CharField(max_length=100, blank=True, null=True)
-    report_file = models.URLField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.created_at}: {self.location.location_name} - {self.hospital.hostpital_name} hospital"
--- hospital_authorities/views.py
+++ hospital_authorities/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import HospitalAuthority
 from .serializers import HospitalAuthoritySerializer
+from rest_framework.response import Response
 
 
 # Create your views here.
 class HospitalAuthorityViewSet(viewsets.ModelViewSet):
     queryset = HospitalAuthority.objects.all()
     serializer_class = HospitalAuthoritySerializer
+
+    # example link: http://127.0.0.1:8000/hospitals/hospital_authorities/by_location/?location_id=3
+    @action(detail=False, methods=['get'])
+    def by_location(self, request):
+        location_id = request.query_params.get('location_id')
+        if not location_id:
+            return Response({"error": "location_id is required"}, status=400)
+
+        hospitals = HospitalAuthority.objects.filter(location_id=location_id)
+        serializer = self.get_serializer(hospitals, many=True)
+        return Response(serializer.data)
 
     
