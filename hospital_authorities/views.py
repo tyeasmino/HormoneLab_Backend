0d7cc170e405215b8da97a62d5e3fa5f66d2d449
Tamima Yeasmin <tyeasmino@gmail.com>
2025-04-04 20:23:47 +0600

    by_locaiton added

--- hospital_authorities/views.py
+++ hospital_authorities/views.py
@@ -6,14 +6,14 @@ from .models import HospitalAuthority
 from .serializers import HospitalAuthoritySerializer
 from marketing_executives.models import MarketingExecutive
 from django.contrib.auth.models import User
-
+from rest_framework import status
 
 class HospitalAuthorityViewSet(viewsets.ModelViewSet):
     queryset = HospitalAuthority.objects.all()
     serializer_class = HospitalAuthoritySerializer
     permission_classes = [IsAuthenticated]  
 
-    # Get hospitals under logged-in user
+    # Get hospitals under logged-in Marketing Executive
     @action(detail=False, methods=['get'])
     def under_me(self, request):
         user = request.user  
@@ -26,6 +26,16 @@ class HospitalAuthorityViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
 
         except MarketingExecutive.DoesNotExist:
-            return Response({"error": "User is not linked to any marketing executive"}, status=400)
+            return Response({"error": "User is not linked to any marketing executive"}, status=status.HTTP_400_BAD_REQUEST)
 
+    # **Filter hospitals by location**
+    @action(detail=False, methods=['get'])
+    def by_location(self, request):
+        location_id = request.query_params.get("location_id")
 
+        if not location_id:
+            return Response({"error": "location_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        hospitals = HospitalAuthority.objects.filter(location_id=location_id)
+        serializer = self.get_serializer(hospitals, many=True)
+        return Response(serializer.data)
