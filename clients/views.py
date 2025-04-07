20694a63b70f65f2081825745f760ce6bba85417
Tamima Yeasmin <tyeasmino@gmail.com>
2025-04-08 04:44:49 +0600

    req file updated 4.44 AM

--- clients/views.py
+++ clients/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.utils.timezone import localtime
 from django.http import HttpResponse
+import requests
 from django.shortcuts import get_object_or_404
 from django.utils.encoding import smart_str
 from datetime import datetime
