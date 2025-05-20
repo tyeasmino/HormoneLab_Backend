6432f5d64d0f61279f6ab78292d865f9eefa748e
tyeasmino <tyeasmino@gmail.com>
2025-05-20 15:20:43 +0600

    Report in Email Done

--- clients/views.py
+++ clients/views.py
@@ -1,39 +1,31 @@
-from rest_framework.permissions import IsAuthenticated  
-from rest_framework import viewsets
-from . import models, serializers  
-from marketing_executives.models import MarketingExecutive
-from hospital_authorities.models import HospitalAuthority
-from django.utils import timezone
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-from django.utils.timezone import localtime
-from django.http import HttpResponse
-import requests
-from django.shortcuts import get_object_or_404
-from django.utils.encoding import smart_str
-from datetime import datetime
 import os
-from rest_framework.parsers import MultiPartParser, FormParser
-
- 
-from docx import Document
-from rest_framework.views import APIView 
-from collections import defaultdict
-from .serializers import UploadedReportSerializer
 import re
+from collections import defaultdict
+from datetime import datetime
+
+import requests
 from cloudinary.uploader import upload
 from cloudinary.utils import cloudinary_url
+from django.core.mail import EmailMultiAlternatives
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.utils.encoding import smart_str
+from django.utils.html import strip_tags
+from django.utils.timezone import localtime
+from docx import Document
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from hospital_authorities.models import HospitalAuthority
+from marketing_executives.models import MarketingExecutive
 
-
-
-
-
-
-
-
-
+from . import models, serializers
+from .serializers import UploadedReportSerializer
 
 
 # Create your views here.
@@ -49,48 +41,112 @@ class AllLocationViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.LocationSerializer
 
 
+
+# // old working one 
+# class ReportViewSet(viewsets.ModelViewSet):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = serializers.ReportsSerializer
+
+#     def get_queryset(self):
+#         # This will return all reports
+#         return models.Reports.objects.all()
+
+#     @action(detail=False, methods=['get'], url_path='today')
+#     def today_reports(self, request):
+#         # Get current time in local timezone
+#         now_local = localtime(timezone.now())
+
+#         # Calculate the boundaries for today in the local timezone
+#         today_start = now_local.replace(hour=0, minute=0, second=0, microsecond=0)
+#         today_end = now_local.replace(hour=23, minute=59, second=59, microsecond=999999)
+ 
+
+#         # Filter reports based on local timezone's boundaries
+#         reports_today = models.Reports.objects.filter(
+#             created_at__gte=today_start,
+#             created_at__lte=today_end
+#         )
+
+#         # Serialize and return today's reports
+#         serializer = self.get_serializer(reports_today, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+#     @action(detail=True, methods=['patch'], url_path='toggle-signed')
+#     def toggle_signed(self, request, pk=None):
+#         try:
+#             report = self.get_object()
+#             report.signed = not report.signed
+#             report.save()
+#             serializer = self.get_serializer(report, context={"request": request})
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         except models.Reports.DoesNotExist:
+#             return Response({'error': 'Report not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+# # testing for email 
 class ReportViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = serializers.ReportsSerializer
 
     def get_queryset(self):
-        # This will return all reports
         return models.Reports.objects.all()
 
-    @action(detail=False, methods=['get'], url_path='today')
-    def today_reports(self, request):
-        # Get current time in local timezone
-        now_local = localtime(timezone.now())
+    def send_report_email(self, report, is_update=False):
+        location = report.location
+        hospital = report.hospital
+        download_link = report.report_file
+        report_name = report.report_name or "Unnamed Report"
+        sender_name = self.request.user.get_full_name() or "HormoneLab"
 
-        # Calculate the boundaries for today in the local timezone
-        today_start = now_local.replace(hour=0, minute=0, second=0, microsecond=0)
-        today_end = now_local.replace(hour=23, minute=59, second=59, microsecond=999999)
- 
-
-        # Filter reports based on local timezone's boundaries
-        reports_today = models.Reports.objects.filter(
-            created_at__gte=today_start,
-            created_at__lte=today_end
-        )
-
-        # Serialize and return today's reports
-        serializer = self.get_serializer(reports_today, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        action_text = "updated" if is_update else "added"
+        subject = f"Lab Report {action_text.capitalize()}: {report_name}"
 
 
-    @action(detail=True, methods=['patch'], url_path='toggle-signed')
-    def toggle_signed(self, request, pk=None):
-        try:
-            report = self.get_object()
-            report.signed = not report.signed
-            report.save()
-            serializer = self.get_serializer(report, context={"request": request})
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except models.Reports.DoesNotExist:
-            return Response({'error': 'Report not found'}, status=status.HTTP_404_NOT_FOUND)
+        if location:
+            try:
+                marketing_executive = MarketingExecutive.objects.get(location=location)
+                if marketing_executive.user.email:
+                    exec_user = marketing_executive.user
+                    exec_name = exec_user.first_name or exec_user.username
+                    html_exec = f"""
+                        <p>Hi {exec_name},</p>
+                        <p>The lab report <strong>{report_name}</strong> report has been <strong>{action_text}</strong>.</p>
+                        <p>You can download it from here: <a href="{download_link}">{download_link}</a></p>
+                        <p>Best Regards,<br>{sender_name}</p>
+                    """
+                    text_exec = strip_tags(html_exec)
+                    email_exec = EmailMultiAlternatives(subject, text_exec, to=[exec_user.email])
+                    email_exec.attach_alternative(html_exec, "text/html")
+                    email_exec.send()
+                else:
+                    print("Marketing executive found but has no email to send.")
+            except MarketingExecutive.DoesNotExist:
+                print("No user is found to send email.")
 
-        
 
+        # Send to Hospital Authority (if exists)
+        if hospital and hospital.user and hospital.user.email:
+            hosp_user = hospital.user
+            hosp_name = hosp_user.first_name or hosp_user.username
+            html_hosp = f"""
+                <p>Hi {hosp_name},</p>
+                <p>The lab report <strong>{report_name}</strong> has been <strong>{action_text}</strong>.</p>
+                <p>You can download it from here: <a href="{download_link}">{download_link}</a></p>
+                <p>Best Regards,<br>{sender_name}</p>
+            """
+            text_hosp = strip_tags(html_hosp)
+            email_hosp = EmailMultiAlternatives(subject, text_hosp, to=[hosp_user.email])
+            email_hosp.attach_alternative(html_hosp, "text/html")
+            email_hosp.send()
+
+    def perform_create(self, serializer):
+        report = serializer.save()
+        self.send_report_email(report, is_update=False)
+
+    def perform_update(self, serializer):
+        report = serializer.save()
+        self.send_report_email(report, is_update=True)
     
 
 
